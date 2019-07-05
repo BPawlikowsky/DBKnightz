1a8d9265e62118870e641090fb15820b3ac96a0b
BPPawlikowsky <bartoszpawlikowsky@gmail.com>
2019-07-06 00:34:15 +0200

    Dodge Ball Knights core - first commit

---
+++ scenes/Player.gd
@@ -0,0 +1,20 @@
+extends KinematicBody2D
+
+export var speed = 40
+
+
+func _process(delta):
+	var velocity = Vector2()
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	
+	
+	velocity = velocity.normalized() * speed
+	move_and_slide(velocity)
+	print(position)
