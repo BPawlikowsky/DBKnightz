f406a76de22ad110a46d8e676513895b3a842bd5
BPPawlikowsky <bartoszpawlikowsky@gmail.com>
2019-07-06 22:33:27 +0200

    Balls work, score works, both players are up, Shield missing

--- scenes/Ball.gd
+++ scenes/Ball.gd
@@ -9,7 +9,9 @@ func _ready():
 		linear_velocity = Vector2(-speed, 0)
 
 
+
 func _process(delta):
+	
 	if global_position.x > get_viewport_rect().size.x + 10:
 		get_parent().get_parent().get_node("Player01").set_score(get_parent().get_parent().get_node("Player01").get_score() + 1)
 		queue_free()
